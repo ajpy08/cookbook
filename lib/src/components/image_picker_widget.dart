86dd7d357cc5cdd70044fea51250f162a5b342ef
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-06-15 10:43:33 -0500

    tmp

---
+++ lib/src/components/image_picker_widget.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+typedef OnImageSelected = Function(File imageFile);
+
+class ImagePickerWidget extends StatefulWidget {
+  final File imageFile;
+  final OnImageSelected onImageSelected;
+
+  ImagePickerWidget({@required this.imageFile, @required this.onImageSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 320,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.cyan[300], Colors.cyan[800]],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight),
+        image: imageFile != null
+            ? DecorationImage(
+                image: FileImage(imageFile),
+                fit: BoxFit.cover,
+              )
+            : null,
+      ),
+      child: IconButton(
+        icon: Icon(Icons.camera_alt),
+        onPressed: () {
+          _showPickerOptions(context);
+        },
+        iconSize: 90,
+      ),
+      color: Colors.white,
+    );
+  }
+
+  _showPickerOptions(BuildContext context) {
+    showDialog(context: context, builder: (BuildContext context) {
+      return SimpleDialog(
+        children: [ListTile(leading: Icon(Icons.camera_alt),
+        title: Text('Camara'),
+        onTap: () {
+          Navigator.pop(context);
+          _showPickImage(context, ImageSource.camera);
+        },
+        ),
+        ListTile(leading: Icon(Icons.image),
+        title: Text('Galeria'),
+        onTap: () {
+          Navigator.pop(context);
+          _showPickImage(context, ImageSource.gallery);
+        },
+        ),
+        ],
+      );
+    },
+    );
+}
+
+@override
+  _ImagePickerWidgetState createState() => _ImagePickerWidgetState();
+
+class _ImagePickerWidgetState extends State<ImagePickerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+void _showPickImage(BuildContext context, camera) {}
+}
